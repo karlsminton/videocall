b2c88a5035e04046b3aeb67d3008aff52d811359
karlsminton <ksm1nn0@gmail>
2022-11-24 13:26:25 +0000

    minor server changes - adding client identifier

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const http = require('http')
 const server = http.createServer(app)
 const { WebSocketServer } = require('ws')
 const path = require('path')
+const crypto = require('crypto')
 
 const ws = new WebSocketServer({ server })
 
-let clients = []
+let clients = {}
 
 // https://stackoverflow.com/questions/69378155/is-it-possible-to-make-a-video-call-over-websocket-on-the-web-without-using-webr
 
@@ -21,7 +22,20 @@ ws.on('connection', (socket, req) => {
     console.log(`req.socket.remoteAddress - ${req.socket.remoteAddress}`)
     // requested url by socket
     let url = req.url
-    clients.push(socket)
+    
+    // give socket a unique id
+    socket.id = crypto.randomUUID()
+
+    // add socket to list of connected clients
+    clients[socket.id] = socket
+
+    // log all connected clients
+    console.log('')
+    console.log('clients connected;')
+    Object.keys(clients).forEach(key => {
+        console.log(`client id - ${clients[key].id}`)
+    })
+    console.log('')
 
     /** @todo extract into different controller classes */
     // video responses
@@ -41,8 +55,8 @@ ws.on('connection', (socket, req) => {
 })
 
 const sendAll = (msg) => {
-    clients.forEach((client) => {
-        client.send(msg)
+    Object.keys(clients).forEach((key) => {
+        clients[key].send(msg)
     })
 }
 
